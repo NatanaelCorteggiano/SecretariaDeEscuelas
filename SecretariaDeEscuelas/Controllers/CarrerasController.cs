701273b728dea22c3ded0b729fc5c0c3bdcc629e
NatanaelCorteggiano <natacorteggiano@hotmail.com>
2020-11-23 14:52:27 -0300

    Se crea la vista Instituciones y Carreras
    Quedan en una misma lista cada institución con su respectiva carrera correspondiente

---
+++ SecretariaDeEscuelas/Controllers/CarrerasController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SecretariaDeEscuelas.Data;
+using SecretariaDeEscuelas.Models;
+
+namespace SecretariaDeEscuelas.Controllers
+{
+    public class CarrerasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CarrerasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Carreras
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Carreras.Include(c => c.Instituto);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        public JsonResult ObtenerCarreras(int instituto)
+        {
+            var carreras = _context.Carreras.ToList().Where(x => x.InstitutoId == instituto);
+            return Json(new SelectList(carreras, "Id", "Nombre"));
+        }
+
+        // GET: Carreras/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carrera = await _context.Carreras
+                .Include(c => c.Instituto)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (carrera == null)
+            {
+                return NotFound();
+            }
+
+            return View(carrera);
+        }
+
+        // GET: Carreras/Create
+        public IActionResult Create()
+        {
+            ViewData["InstitutoId"] = new SelectList(_context.Institutos, "Id", "Id");
+            return View();
+        }
+
+        // POST: Carreras/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,InstitutoId")] Carrera carrera)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(carrera);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["InstitutoId"] = new SelectList(_context.Institutos, "Id", "Id", carrera.InstitutoId);
+            return View(carrera);
+        }
+
+        // GET: Carreras/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carrera = await _context.Carreras.FindAsync(id);
+            if (carrera == null)
+            {
+                return NotFound();
+            }
+            ViewData["InstitutoId"] = new SelectList(_context.Institutos, "Id", "Id", carrera.InstitutoId);
+            return View(carrera);
+        }
+
+        // POST: Carreras/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,InstitutoId")] Carrera carrera)
+        {
+            if (id != carrera.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(carrera);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CarreraExists(carrera.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["InstitutoId"] = new SelectList(_context.Institutos, "Id", "Id", carrera.InstitutoId);
+            return View(carrera);
+        }
+
+        // GET: Carreras/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carrera = await _context.Carreras
+                .Include(c => c.Instituto)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (carrera == null)
+            {
+                return NotFound();
+            }
+
+            return View(carrera);
+        }
+
+        // POST: Carreras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var carrera = await _context.Carreras.FindAsync(id);
+            _context.Carreras.Remove(carrera);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CarreraExists(int id)
+        {
+            return _context.Carreras.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SecretariaDeEscuelas/Controllers/InstitutosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SecretariaDeEscuelas.Data;
+using SecretariaDeEscuelas.Models;
+
+namespace SecretariaDeEscuelas.Controllers
+{
+    public class InstitutosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InstitutosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Institutos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Institutos.ToListAsync());
+        }
+
+        // GET: Institutos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instituto = await _context.Institutos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (instituto == null)
+            {
+                return NotFound();
+            }
+
+            return View(instituto);
+        }
+
+        // GET: Institutos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Institutos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre")] Instituto instituto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(instituto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instituto);
+        }
+
+        // GET: Institutos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instituto = await _context.Institutos.FindAsync(id);
+            if (instituto == null)
+            {
+                return NotFound();
+            }
+            return View(instituto);
+        }
+
+        // POST: Institutos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre")] Instituto instituto)
+        {
+            if (id != instituto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(instituto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InstitutoExists(instituto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instituto);
+        }
+
+        // GET: Institutos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var instituto = await _context.Institutos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (instituto == null)
+            {
+                return NotFound();
+            }
+
+            return View(instituto);
+        }
+
+        // POST: Institutos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var instituto = await _context.Institutos.FindAsync(id);
+            _context.Institutos.Remove(instituto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InstitutoExists(int id)
+        {
+            return _context.Institutos.Any(e => e.Id == id);
+        }
+    }
+}
